18e8b262c9afa007e47b38dbd555ff11b9e6083a
Andressa Mourão <am.andressamouraom@gmail.com>
2022-02-10 15:41:28 -0300

    first commit

---
+++ passgen.js
@@ -0,0 +1,21 @@
+let password = document.getElementById("password");
+
+function genPassword() {
+  let characters =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordLength = 12;
+  let password = "";
+
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber + 1);
+  }
+
+  document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    let copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");  
+  }
